228bd87ee0cdfef74f8002c2810f3070711a2c42
katerina <ekaterina.pomitova@mail.ru>
2022-01-23 18:49:12 +0300

    config constructor binding

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgdi;
 
 import guru.springframework.sfgdi.config.SfgConfig;
+import guru.springframework.sfgdi.config.SfgConstructorConfig;
 import guru.springframework.sfgdi.controllers.*;
 import guru.springframework.sfgdi.darasource.FakeDataSource;
 import guru.springframework.sfgdi.services.PrototypeBean;
@@ -57,11 +58,17 @@ public class SfgDiApplication {
 		System.out.println(fakeDataSource.getPassword());
 		System.out.println(fakeDataSource.getJdbcurl());
 
-		System.out.println("------------ Sfg Config ------------");
+		System.out.println("------------ Config Props Bean ------------");
 		SfgConfig sfgConfig = ctx.getBean(SfgConfig.class);
 		System.out.println(sfgConfig.getUsername());
 		System.out.println(sfgConfig.getPassword());
 		System.out.println(sfgConfig.getJdbcurl());
+
+		System.out.println("------------ Constructor Binding ------------");
+		SfgConstructorConfig sfgConstructorConfig = ctx.getBean(SfgConstructorConfig.class);
+		System.out.println(sfgConstructorConfig.getUsername());
+		System.out.println(sfgConstructorConfig.getPassword());
+		System.out.println(sfgConstructorConfig.getJdbcurl());
 	}
 
 }
--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -6,20 +6,21 @@ import guru.springframework.sfgdi.darasource.FakeDataSource;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.*;
 
+@EnableConfigurationProperties(SfgConstructorConfig.class)
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
 
     @Bean
-    FakeDataSource fakeDataSource(SfgConfig sfgConfig) {
+    FakeDataSource fakeDataSource(SfgConstructorConfig sfgConstructorConfig) {
 
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUsername(sfgConfig.getUsername());
-        fakeDataSource.setPassword(sfgConfig.getPassword());
-        fakeDataSource.setJdbcurl(sfgConfig.getJdbcurl());
+        fakeDataSource.setUsername(sfgConstructorConfig.getUsername());
+        fakeDataSource.setPassword(sfgConstructorConfig.getPassword());
+        fakeDataSource.setJdbcurl(sfgConstructorConfig.getJdbcurl());
         return fakeDataSource;
     }
 
